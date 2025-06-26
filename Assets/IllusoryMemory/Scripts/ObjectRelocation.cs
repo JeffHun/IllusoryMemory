ffd9a36e1fdf15d6bfd68ade3140793e1a822dfa
JeffHun <lecrubier.hugo@gmail.com>
2025-06-26 16:10:09 +0200

    [Feat] Mug relocation

--- Assets/IllusoryMemory/Scripts/ObjectRelocation.cs
+++ Assets/IllusoryMemory/Scripts/ObjectRelocation.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Phone : MonoBehaviour
+public class ObjectRelocation : MonoBehaviour
 {
+    [SerializeField]
+    string _tag;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Phone")
+        if (other.gameObject.tag == _tag)
         {
             other.transform.position = transform.position;
             other.transform.rotation = transform.rotation;
